e81a526db24857041ead308b9405dd3d6f187b40
saamaaraii <94558655+saamaaraiii@users.noreply.github.com>
2025-10-28 13:34:04 +0000

    Add files via upload

---
+++ reaction-time-experiment-main/README.md
@@ -0,0 +1,41 @@
+# Exercise E1 – Human Capabilities - Reaction Time Experiments
+
+Here we provide the resources for running the Reaction Time experiments. 
+There are two experiments: Visual Reaction and Auditive Reaction.
+
+
+No code edits are required for this exercise. You are expected to run it as is.
+
+## Visual Reaction Time Experiment:
+
+The visual reaction test works as follows:  
+
+←   The application shows a coloured circle as stimulus.
+←   Click the "Start Experiment" button to start experiment.
+←   After you start the experiment, the colour of the stimulus will change at a random time between 2 and 5 seconds.
+←   Your task is to react by pressing the SPACE bar.  
+←   The program measures the time from the colour change until the SPACE bar is hit and displays it on the screen.  
+←   The experiment ends after 10 trials, and the reaction times are exported in a .csv file. 
+
+## Auditive Reaction Time Experiment:
+
+The audtive reaction test works as follows:  
+
+←   The application will play a sound as stimulus.
+←   Click the "Start Experiment" button to start experiment.
+←   After you start the experiment, the sound stimulus will be played at a random time between 2 and 5 seconds.
+←   Your task is to react by pressing the SPACE bar.  
+←   The program measures the time from the start of sound playing until the SPACE bar is hit and displays it on the screen.  
+←   The experiment ends after 10 trials, and the reaction times are exported in a .csv file. 
+
+##Excercise Task:
+Try out each application first, and then use them to collect data on your reaction time. 
+Import into excel and calculate means:
+  - Compare it to the mean values shown at end of experiment
+  – Is there any difference in your reaction time depending on the type of stimulus?  
+
+**Bring the data to the seminar for further analysis.**
+
+_Created By Abe Karnik_
+
+_Updated for 24-25 By Dan Fitton_
---
+++ reaction-time-experiment-main/script.js
@@ -0,0 +1,198 @@
+const experiment = {
+  times: [],
+  maxTrials: 10,
+  waitHnd: -1,
+  started: false,
+  ended: false,
+  stimulusWait: false,
+  stimulusShown: false,
+  stimulusShownAt: -1,
+  btnDisabled:false
+};
+
+const btn = document.querySelector(".button-default");
+const stimulus = document.querySelector(".circle");
+
+const advanceTrial = function () {
+  //reset stimulus
+  updateStimulus("inactive");
+
+  if (experiment.times.length < experiment.maxTrials) {
+    //still need to run more trials
+    experiment.stimulusShown = false; //reset
+    scheduleStimulus();
+  } else {
+    //experiment ended
+    experiment.stimulusShown = false;
+    endExperiment();
+  }
+};
+
+const endExperiment = function () {
+  console.info("INFO: Experiment ended. Await download of results");
+
+  experiment.ended = true;
+
+  //Update Button Styling
+  experiment.btnDisabled = false;
+  btn.classList.toggle("button-enabled");
+  btn.classList.toggle("button-disabled");
+  btn.textContent = "Download Data";
+};
+
+const scheduleStimulus = function () {
+  experiment.stimulusWait = true;
+  const randomDelay = Math.floor(Math.random() * 4 + 2); // 2 - 5s
+  experiment.waitHnd = window.setTimeout(showStimulus, randomDelay * 1000); //setTimeout runs in milliseconds
+  console.info(
+    "INFO: Trial",
+    experiment.times.length,
+    ". Random delay:",
+    randomDelay
+  );
+};
+
+const showStimulus = function () {
+  experiment.stimulusShownAt = Date.now();
+  console.info(
+    "INFO: Trial",
+    experiment.times.length,
+    ". Stimulus shown",
+    experiment.stimulusShownAt
+  );
+  updateStimulus("active");
+  experiment.stimulusWait = false;
+  experiment.stimulusShown = true;
+};
+
+const updateStimulus = function (state) {
+  const otherState = state == "active" ? "inactive" : "active";
+
+  stimulus.classList.add(state);
+  stimulus.classList.remove(otherState);
+};
+
+const logReaction = function () {
+  let userReactedAt = Date.now();
+  console.info("INFO: User reaction captured.", userReactedAt);
+
+  let deltaTime = userReactedAt - experiment.stimulusShownAt;
+  experiment.times.push(deltaTime);
+  document.querySelector("#time").textContent = deltaTime + " ms";
+};
+
+const userReaction = function () {
+  if (!experiment.started) {
+    return;
+  } //prior to start of experiment, ignore
+  if (experiment.stimulusWait) {
+    return;
+  } //ignore false trigger reactions
+
+  if (experiment.stimulusShown) {
+    //stimulus is visible, capture
+    logReaction();
+    advanceTrial();
+  }
+};
+
+const startExperiment = function () {
+  console.info("INFO: Experiment Started");
+  stimulus.style = "background-color:'';";
+  document.querySelector("#instructions").style.display = "none";
+  experiment.started = true;
+  window.addEventListener("keypress", onKey); //add keylistener
+  advanceTrial();
+};
+
+const btnAction = function () {
+  console.debug("DBG:", "click");
+  if(experiment.btnDisabled) return;
+  if (!experiment.ended) {
+    experiment.btnDisabled = true;
+    btn.classList.toggle("button-enabled");
+    btn.classList.toggle("button-disabled");
+  }
+  if (!experiment.started) {
+    startExperiment();
+  } else {
+    if (experiment.ended) {
+      exportExperimentLog();
+      const stats = computeStatistics(experiment.times);
+      document.querySelector("#time").textContent = [
+        "Count:",
+        stats.cnt,
+        "Mean:",
+        stats.mean.toFixed(2),
+        "ms",
+        "SD:",
+        stats.sd.toFixed(2),
+        "ms",
+      ].join(" ");
+    } else {
+      console.log("DBG: Should this occur?");
+    }
+  }
+};
+
+const computeStatistics = function (timeArr) {
+  //to get mean, get sum of all trials and divide by number of trials m = sum(x)/cnt(x)
+  const sums = timeArr.reduce((acc, num) => acc + num, 0);
+  const meanDeltaTime = sums / timeArr.length;
+
+  //standard deviation is  sqrt(sum(x-mean)^2/cnt(x))
+  const squaredDiffs = timeArr.reduce(
+    (acc, num) => (num - meanDeltaTime) ** 2 + acc,
+    0
+  );
+  const standardDeviationTime = Math.sqrt(squaredDiffs / timeArr.length);
+
+  return {
+    sd: standardDeviationTime,
+    mean: meanDeltaTime,
+    cnt: timeArr.length,
+  };
+};
+
+const exportExperimentLog = function () {
+  let csvHeader = "pid,trial#,reactionTime (ms)\n";
+  let pid = Math.floor(Math.random() * 900000) + 100000;
+  let csvData = experiment.times
+    .map((time, idx) => [pid, idx, time].join(","))
+    .join("\n"); //map passes every record in the log array to the getCSVDataLine, we also need to include pid to all rows
+  exportData(csvHeader + csvData, "DominantReactionTestResults.csv");
+};
+
+const exportData = function (csvDataString, exportFileName) {
+  // Create a Blob with the CSV data
+  const blob = new Blob([csvDataString], { type: "text/csv" });
+
+  // Create a temporary link element
+  const a = document.createElement("a");
+  a.href = window.URL.createObjectURL(blob);
+  a.download = exportFileName;
+
+  // Trigger the download
+  document.body.appendChild(a);
+  a.style.display = "none";
+  a.click();
+
+  // Clean up
+  window.URL.revokeObjectURL(a.href);
+  document.body.removeChild(a);
+};
+
+const onKey = function (evt) {
+  if (evt == null) {
+    evt = window.event;
+  }
+  switch (evt.which || evt.charCode || evt.keyCode) {
+    case 32: //space
+      userReaction();
+      break;
+    default:
+      console.warn("WARN: Key:", evt, evt.which, evt.charCode, evt.keyCode);
+  }
+};
+
+btn.addEventListener("click", btnAction);
